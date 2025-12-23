fb8d9a3541ba974208efea457cc60c0c92080ad0
tanvir1Ahmed <tanvir.ahmed.ta.khan@gmail.com>
2025-12-23 15:34:51 +0600

    .

--- Quiz_Portal/Program.cs
+++ Quiz_Portal/Program.cs
@@ -33,16 +33,18 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+// Map API controllers
+app.MapControllers();
 
+// Map MVC routes
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
